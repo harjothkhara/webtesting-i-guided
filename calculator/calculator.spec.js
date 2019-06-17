e33ef1ba91287cb7556fa4fdd39579cc2afd6b17
harjothkhara <harjoth.khara@gmail.com>
2019-06-16 19:44:02 -0700

    jest setup, running first test

--- calculator/calculator.spec.js
+++ calculator/calculator.spec.js
@@ -1,3 +1,15 @@
 const { add } = require('./calculator.js');
 
 // test away!
+
+// test case
+it('runs the tests', () => {
+    // arrange - setup
+    const expected = true;
+
+    // act - run the system under test
+    const actual = true;
+    
+    // assert - verify that it works
+    expect(actual).toBe(expected)
+});
